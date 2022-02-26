8af5c274a7d9870c3f30f1925022d32dae328810
이형종 <hj36879@naver.com>
2022-02-26 16:02:57 +0900

    나이 계산 공식 결과를 리스트뷰에 반영

--- app/src/main/java/com/hyeongjong/listview_20220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/hyeongjong/listview_20220226/adapters/StudentAdapter.kt
@@ -44,6 +44,9 @@ class StudentAdapter(
         txtStudentName.text = data.name
         txtAddress.text = data.address
 
+        val age = 2022 - data.birthYear + 1
+        txtAge.text = "(${age}세)"
+
 //        반영된 채로 결과로 나가게
 
         return row
