b391be2e1497f9c47dcc6855a3f0de997e6690e2
이형종 <hj36879@naver.com>
2022-02-26 15:13:54 +0900

    student_list_item xml 파일을, 코틀린에서 활용할 수 있는 준비(tempRow에 담아두기)

--- app/src/main/java/com/hyeongjong/listview_20220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/hyeongjong/listview_20220226/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.hyeongjong.listview_20220226.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.hyeongjong.listview_20220226.R
 import com.hyeongjong.listview_20220226.datas.StudentData
 
 class StudentAdapter(
@@ -10,4 +14,15 @@ class StudentAdapter(
     val mList : ArrayList<StudentData> //실제로 뿌려줄 학생들은 누구누구? 목록
 
     ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+
+        }
+    }
 }
