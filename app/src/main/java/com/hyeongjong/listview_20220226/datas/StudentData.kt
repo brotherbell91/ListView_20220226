af7debe7866ef920821bb41473e7e7bd15b10f80
이형종 <hj36879@naver.com>
2022-02-27 11:09:07 +0900

    연도를 기반으로 > 나이 계산하는 함수 추가

--- app/src/main/java/com/hyeongjong/listview_20220226/datas/StudentData.kt
+++ app/src/main/java/com/hyeongjong/listview_20220226/datas/StudentData.kt
@@ -6,4 +6,14 @@ class StudentData(
     val address: String,
 
 ) {
+    //    연도를 받아서 > 그 해의 나이를 계산해주는 함수
+
+    fun getKoreanAge( year: Int ) : Int {
+
+        val age =  year - birthYear + 1
+
+        return age
+
+    }
+
 }
